e6647f01521a35b076915b59da482ca2279aca8e
Pulse <745653239@qq.com>
2020-04-21 22:58:05 +0800

    新增Ant Design Mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { Button } from "antd";
-import "./App.css";
+import React, { Component } from "react";
+import { Button } from "antd-mobile";
+import styles from "./App.css";
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <p className={styles["App-intro"]}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button type="primary">This is a button</Button>
+      </div>
+    );
+  }
+}
 
 export default App;
